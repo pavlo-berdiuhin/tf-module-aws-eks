fa1b3064e61c37cf088b3d32c6c41ecb49fc7c5c
Pavlo Berdiuhin <p.berdyuhin@gmail.com>
2025-05-09 17:54:43 +0200

    use kubectl provider

--- versions.tf
+++ versions.tf
@@ -10,9 +10,9 @@ terraform {
       source  = "hashicorp/helm"
       version = ">= 2.7"
     }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = ">= 2.27"
+    kubectl = {
+      source  = "alekc/kubectl"
+      version = ">= 2.0"
     }
   }
 }
